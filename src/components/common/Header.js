663a3cb63f37ec75b7e374d5823147f4c157bf9e
JackEdwardLyons <jack.edward.lyons@gmail.com>
2018-10-10 17:25:12 +1000

    feat: Add List.js component to render list of currencies.
    Currencies are grabbed from an API endpoint and will be displayed on
    the screen. Testing with console.log here which seems to work fine.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ import logo from './logo.png'
 const Header = () => {
   return (
     <div className="Header">
-      <img src={ logo } className="Header-logo" alot="React Coin Logo" />
+      <img src={ logo } className="Header-logo" alt="React Coin Logo" />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/common/Header'
+import List from './components/list/List'
+
 import './index.css'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <List />
       <h1>Hello World</h1>
     </div>
   )
