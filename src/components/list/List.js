f20dfd4c774521656c18b98b48255dd2323da2db
JackEdwardLyons <jack.edward.lyons@gmail.com>
2018-10-11 15:55:11 +1000

    refactor: Add proptypes check for table component and separate into new file
    Minor refactoring and prop type checks to ensure app runs as expected.

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { handleResponse } from '../../helpers'
 import { API_URL } from '../../config'
 import Loading from '../common/Loading'
-import './Table.css'
+import Table from './Table'
 
 class List extends React.Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       loading: false,
@@ -16,7 +16,7 @@ class List extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ loading: true });
+    this.setState({ loading: true })
     
     fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
       .then(handleResponse)
@@ -24,14 +24,14 @@ class List extends React.Component {
         this.setState({
           currencies: data.currencies,
           loading: false,
-        });
+        })
       })
       .catch((error) => {
         this.setState({
           error: error.errorMessage,
           loading: false,
-        });
-      });
+        })
+      })
   }
 
   renderChangePercent(percent) {
@@ -54,44 +54,15 @@ class List extends React.Component {
 
     // render only error message, if error occurred while fetching data
     if (error) {
-      return <div className="error">{error}</div>
+      return <div className="error">{ error }</div>
     }
 
     return (
-      <div className="Table-container"> 
-        <table className="Table">
-          <thead className="Table-head">
-            <tr>
-              <th>Cryptocurrency</th>
-              <th>Price</th>
-              <th>Market Cap</th>
-              <th>24H Change</th>  
-            </tr>    
-          </thead>
-          <tbody className="Table-body">
-          { 
-            currencies.map((currency) => (
-              <tr key={ currency.id }>
-                <td>
-                  <span className="Table-rank">{ currency.rank }</span>
-                  { currency.name }
-                </td>
-                <td>
-                  <span className="Table-dollar">$ {currency.price}</span>
-                </td>
-                <td>
-                  <span className="Table-dollar">$ {currency.marketCap}</span>
-                </td>
-                <td>
-                  { this.renderChangePercent(currency.percentChange24h) }
-                </td>
-              </tr>
-            ))
-          }
-          </tbody>
-        </table>
-      </div>
-    );
+      <Table 
+        currencies={ currencies } 
+        renderChangePercent={ this.renderChangePercent } 
+      />
+    )
   }
 }
 
